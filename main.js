ba580f5e08c8d7f3ae237e8d1801c3858350e8a2
Kerem <85187618+Keremy-PRO@users.noreply.github.com>
2023-04-08 14:02:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
-background_image="mars.jpg"
-rover_image="rover.png"
+background_image="mars.jpg";
+rover_image="rover.png";
+
+x = 10;
+y = 10;
 function add (){
    background_imgTag=new Image();
    background_imgTag.onload=uploadbackground;
-   background_imgTag.src=background_image
+   background_imgTag.src=background_image;
+   
+     rover_imgTag=new Image();
+    rover_imgTag.onload=uploadrover;
+    rover_imgTag.src=background_image;
 }
 function uploadbackground(){
 ctx.drawImage(background_imgTag,0,0,canvas.witdth,canvas.height);
 }
-function add (){
-   rover_imgTag.onload=uploadrover;
-    rover_imgTag.src=background_image;
-}
+
 function uploadrover (){
     ctx=drawImage(rover_imgTag,0,0,x,y,100,100)
     rover_imgTag = new Image();
