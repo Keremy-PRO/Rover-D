75ee401cf9e1f82787a2febcd27d19fa5de62c8e
Kerem <85187618+Keremy-PRO@users.noreply.github.com>
2023-04-08 14:05:50 -0600

    Update main.js
    A ver, checa esta versión Matias, estoy segura que ya funciona

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
-background_image="mars.jpg"
-rover_image="rover.png"
-function add (){
-   background_imgTag=new Image();
-   background_imgTag.onload=uploadbackground;
-   background_imgTag.src=background_image
+
+rover_width = 100;
+rover_height = 90;
+
+background_image = "mars.jpg";
+
+rover_image = "rover.png";
+
+rover_x = 10;
+rover_y = 10;
+
+function add() {
+	background_imgTag = new Image(); 
+	background_imgTag.onload = uploadBackground; 
+	background_imgTag.src = background_image;   
+
+	rover_imgTag = new Image(); 
+	rover_imgTag.onload = uploadrover; 
+	rover_imgTag.src = rover_image;   
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
-function uploadbackground(){
-ctx.drawImage(background_imgTag,0,0,canvas.witdth,canvas.height);
-}
-function add (){
-   rover_imgTag.onload=uploadrover;
-    rover_imgTag.src=background_image;
-}
-function uploadrover (){
-    ctx=drawImage(rover_imgTag,0,0,x,y,100,100)
-    rover_imgTag = new Image();
+
+function uploadrover() {
+	ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
 }
